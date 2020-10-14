341c64b18dea183b917449aaa802255f17355952
Erik Fernandes <erikfernandes729@gmail.com>
2020-10-14 15:02:48 -0400

    Added views for cart, checkout, orders. Updated controllers

--- routes/admin.js
+++ routes/admin.js
@@ -3,14 +3,16 @@ const path = require('path');
 const express = require('express');
 
 // Controller imports
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
 // /admin/add-product ==> GET
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
 
 // /admin/add-product ==> POST
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
+
+router.get('/all-products', adminController.getProducts);
 
 module.exports = router;
